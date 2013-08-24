70d7b1f28912a96a5ddc3cdc2df2e67c75a3f0e0
beenje <beenje@gmail.com>
2013-08-24 22:48:09 +0200

    Fix #1 (the way the media URL is constructed)
    When accessing programs via "Toutes les émissions", the prefix is not part
    of the href tag (as before), but when going directly to "@rrêt sur images",
    the prefix is part of the href.
    We check if the href starts with "http" or not.
    
    Thanks to mossroy!

--- resources/lib/scraper.py
+++ resources/lib/scraper.py
@@ -129,7 +129,10 @@ class Programs:
         for media in soup.findAll('div', {'class': 'contenu-descr-8 '}):
             tag = media.findPrevious('a')
             # Get link, title and thumb
-            media_link = URLASI + tag['href']
+            if tag['href'].startswith('http'):
+                media_link = tag['href']
+            else:
+                media_link = URLASI + tag['href']
             media_title = tag['title'].encode('utf-8')
             media_thumb = URLASI + tag.find('img', attrs={'src': re.compile('.+?\.[png|jpg]')})['src']
             yield {'url': media_link, 'title': media_title, 'thumb': media_thumb}
